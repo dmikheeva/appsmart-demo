eba4a8fd119f95e5767a0c404739a6894e03e958
dmikheeva <daria.mikheeva@ibm.com>
2022-02-16 16:11:27 +0300

    remove redundant declaration

--- src/main/java/com/appsmart/demo/repository/ProductRepository.java
+++ src/main/java/com/appsmart/demo/repository/ProductRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    public Page<Product> findByCustomerId(Long customerId, Pageable pageable);
+    Page<Product> findByCustomerId(Long customerId, Pageable pageable);
 }
